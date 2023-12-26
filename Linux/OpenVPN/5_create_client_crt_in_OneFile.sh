a4914705ee2ad2e7dff72beb39c70af622e7f99f
Slivak_win11 <eas1804@gmail.com>
2023-12-26 13:51:49 +0200

    add OpenVPN and RSAT

---
+++ Linux/OpenVPN/5_create_client_crt_in_OneFile.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+KEY_FOLDER=/etc/openvpn/client/
+USER_NAME=$1
+NO_ROOT_FOLDER=/home/exch_adm/OpenVPN_Client/
+
+ls $KEY_FOLDER || mkdir -p $KEY_FOLDER
+ls /etc/openvpn/ccd || mkdir /etc/openvpn/ccd
+ls $NO_ROOT_FOLDER || mkdir -p $NO_ROOT_FOLDER
+
+clear
+echo "New user is:  $USER_NAME"
+cd /usr/share/easy-rsa/
+echo |./easyrsa gen-req $USER_NAME nopass
+echo "yes"|./easyrsa sign-req client  $USER_NAME
+
+
+mkdir $KEY_FOLDER/$USER_NAME
+cp /usr/share/easy-rsa/pki/private/$USER_NAME.*  $KEY_FOLDER/$USER_NAME
+cp /usr/share/easy-rsa/pki/issued/$USER_NAME.*   $KEY_FOLDER/$USER_NAME
+cp /etc/openvpn/ca.crt   $KEY_FOLDER/$USER_NAME
+cp  /etc/openvpn/ta.key  $KEY_FOLDER/$USER_NAME
+echo "# disable" > /etc/openvpn/ccd/$USER_NAME
+
+# generatee file.ovpn
+IP=$(curl ifconfig.me)
+result=$(iptables -L INPUT -nv | grep OpenVPN)
+PORT=$(echo "$result" | awk '{print $11}' | cut -d: -f2)
+
+echo "remote $IP $PORT" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "cert $USER_NAME.crt" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "key $USER_NAME.key" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+### constanta
+echo "client" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "dev tun" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "proto udp" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "resolv-retry infinite" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "nobind" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "persist-key" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "persist-tun" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "remote-cert-tls server" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "key-direction 1" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "cipher AES-256-GCM" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "auth SHA256" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "verb 3" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "ca ca.crt" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "tls-auth ta.key 1" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+
+##### Part 2. Create Opne File
+cd $KEY_FOLDER/$USER_NAME
+
+KEY_DIR=$KEY_FOLDER/$USER_NAME
+OUTPUT_DIR=$KEY_FOLDER/$USER_NAME
+BASE_CONFIG=./$USER_NAME.ovpn
+
+
+cat ${BASE_CONFIG} \
+<(echo -e '<ca>') \
+${KEY_DIR}/ca.crt \
+<(echo -e '</ca>\n<cert>') \
+${KEY_DIR}/${1}.crt \
+<(echo -e '</cert>\n<key>') \
+${KEY_DIR}/${1}.key \
+<(echo -e '</key>\n<tls-auth>') \
+${KEY_DIR}/ta.key \
+<(echo -e '</tls-auth>') \
+> ${OUTPUT_DIR}/${1}_one_file.ovpn
