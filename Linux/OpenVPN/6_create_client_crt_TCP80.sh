e18635f06028835075e3e0d04bd3d543a74a4312
Slivak_win11 <eas1804@gmail.com>
2024-05-18 11:39:52 +0300

    openvpn tcp

---
+++ Linux/OpenVPN/6_create_client_crt_TCP80.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+ls /etc/openvpn/client/ || mkdir /etc/openvpn/client/
+ls /etc/openvpn/ccd || mkdir /etc/openvpn/ccd
+
+KEY_FOLDER=/etc/openvpn/client/
+USER_NAME=$1
+
+clear
+echo "New user is:  $USER_NAME"
+cd /usr/share/easy-rsa/
+echo |./easyrsa gen-req $USER_NAME nopass
+echo "yes"|./easyrsa sign-req client  $USER_NAME
+
+
+mkdir $KEY_FOLDER/$USER_NAME
+cp /usr/share/easy-rsa/pki/private/$USER_NAME.*  $KEY_FOLDER/$USER_NAME
+cp /usr/share/easy-rsa/pki/issued/$USER_NAME.*   $KEY_FOLDER/$USER_NAME
+cp /etc/openvpn/ca.crt   $KEY_FOLDER/$USER_NAME
+cp  /etc/openvpn/ta.key  $KEY_FOLDER/$USER_NAME
+echo "# disable" > /etc/openvpn/ccd/$USER_NAME
+
+# generatee file.ovpn
+IP=$(curl ifconfig.me)
+PORT=80
+ 
+echo "remote $IP $PORT" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "cert $USER_NAME.crt" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "key $USER_NAME.key" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+
+### constanta
+echo "client" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "dev tun" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "proto tcp" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "resolv-retry infinite" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "nobind" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "persist-key" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "persist-tun" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "remote-cert-tls server" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "key-direction 1" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "cipher AES-256-GCM" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "auth SHA256" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "verb 3" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "ca ca.crt" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+echo "tls-auth ta.key 1" >> $KEY_FOLDER/$USER_NAME/$USER_NAME.ovpn
+
+
+
+echo -----------------------
+echo "/etc/openvpn/client/"
+ls -l /etc/openvpn/client/$1
+
+
+
+
---
+++ Linux/OpenVPN/7_server-conf_tcp_80.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# create congif server
+mkdir /etc/openvpn/ccd
+FILE_SRV=/etc/openvpn/server_tcp.conf
+PORT=80
+
+
+echo "port $PORT"  >$FILE_SRV
+echo "proto tcp" >>$FILE_SRV
+echo "dev tun" >>$FILE_SRV
+echo "ca ca.crt" >>$FILE_SRV
+echo "cert server.crt" >>$FILE_SRV
+echo "key server.key" >>$FILE_SRV
+echo "tls-auth ta.key 0" >>$FILE_SRV
+echo "key-direction 0" >>$FILE_SRV
+echo "cipher AES-256-GCM" >>$FILE_SRV
+echo "auth SHA256" >>$FILE_SRV
+echo "client-to-client" >>$FILE_SRV
+echo "dh dh.pem" >>$FILE_SRV
+echo "user nobody" >>$FILE_SRV
+echo "group nogroup" >>$FILE_SRV
+echo "server 10.88.77.0 255.255.255.0" >>$FILE_SRV
+echo "keepalive 10 120" >>$FILE_SRV
+echo "persist-key" >>$FILE_SRV
+echo "persist-tun" >>$FILE_SRV
+echo "status /var/log/openvpn/openvpn-status_tcp.log" >>$FILE_SRV
+echo "log    /var/log/openvpn/openvpn_tcp.log" >>$FILE_SRV
+echo "ifconfig-pool-persist /var/log/openvpn/ipp_tcp.txt" >>$FILE_SRV
+echo "verb 3" >>$FILE_SRV
+echo 'push "route 192.168.1.0 255.255.255.0"' >>$FILE_SRV
+
+echo 'client-config-dir "/etc/openvpn/ccd"' >>$FILE_SRV
+echo "topology subnet" >>$FILE_SRV
+echo "management /run/ovpn-mgmt unix" >>$FILE_SRV
+echo "tls-server" >>$FILE_SRV
+
+
+echo 'push "redirect-gateway def1 bypass-dhcp"' >>$FILE_SRV
+echo 'push "dhcp-option DNS 208.67.222.222"' >>$FILE_SRV
+echo 'push "dhcp-option DNS 208.67.220.220"' >>$FILE_SRV
+
+cat $FILE_SRV
+
+systemctl start openvpn@server_tcp
+systemctl status openvpn@server_tcp
+cat /var/log/openvpn/openvpn_tcp.log
+
+ip a | grep tun
+
+exit 0
+
