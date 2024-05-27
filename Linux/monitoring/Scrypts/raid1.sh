689a80bcd59d4c55a462cb1ada389fe2244dbf5d
Ievgen Slivak <eas1804@gmail.com>
2024-05-27 09:22:43 +0300

    Proxmox

--- Linux/monitoring/Scrypts/raid1.sh
+++ Linux/monitoring/Scrypts/raid1.sh
@@ -11,16 +11,16 @@ IP_global=$(curl 2ip.ru)
 MESS="Проблема с RAID  $0
 WAN: $IP_global"
 
-# Проверяем вывод команды mdadm -D /dev/md0 на наличие строки "removed"
-if mdadm -D /dev/md0 | grep -q "removed"; then
+# Проверяем вывод команды /usr/sbin/mdadm -D /dev/md0 на наличие строки "removed"
+if /usr/sbin/mdadm -D /dev/md0 | grep -q "removed"; then
     echo "Проблема: удаленные устройства обнаружены в массиве RAID /dev/md0"
     curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&text=$MESS" > /dev/null
 else
     echo "ОК: удаленных устройств не обнаружено в массиве RAID /dev/md0"
 fi
 
-# Проверяем вывод команды mdadm -D /dev/md1 на наличие строки "removed"
-if mdadm -D /dev/md1 | grep -q "removed"; then
+# Проверяем вывод команды /usr/sbin/mdadm -D /dev/md1 на наличие строки "removed"
+if /usr/sbin/mdadm -D /dev/md1 | grep -q "removed"; then
     echo "Проблема: удаленные устройства обнаружены в массиве RAID /dev/md1"
     curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&text=$MESS" > /dev/null
 else
@@ -28,7 +28,7 @@ else
 fi
 
 
-if mdadm -D /dev/md2 | grep -q "removed"; then
+if /usr/sbin/mdadm -D /dev/md2 | grep -q "removed"; then
     echo "Проблема: удаленные устройства обнаружены в массиве RAID /dev/md2"
     curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&text=$MESS" > /dev/null
 else
@@ -36,7 +36,7 @@ else
 fi
 
 
-if mdadm -D /dev/md3 | grep -q "removed"; then
+if /usr/sbin/mdadm -D /dev/md3 | grep -q "removed"; then
     echo "Проблема: удаленные устройства обнаружены в массиве RAID /dev/md3"
     curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&text=$MESS" > /dev/null
 else
--- Linux/monitoring/Scrypts/refresh_home_mlp_pp_ua.sh
+++ Linux/monitoring/Scrypts/refresh_home_mlp_pp_ua.sh
@@ -3,8 +3,8 @@
 DOMEN=home.mlp.pp.ua
 
 RESULT=$(dig $DOMEN A +short)
-iptables -D INPUT -p tcp -s $RESULT -m tcp --dport 22 -j ACCEPT -m comment --comment "$DOMEN"  2>/dev/nul
-iptables -A INPUT -p tcp -s $RESULT -m tcp --dport 22 -j ACCEPT -m comment --comment "$DOMEN"
+/usr/sbin/iptables -D INPUT -p tcp -s $RESULT -m tcp --dport 22 -j ACCEPT -m comment --comment "$DOMEN"  2>/dev/nul
+/usr/sbin/iptables -A INPUT -p tcp -s $RESULT -m tcp --dport 22 -j ACCEPT -m comment --comment "$DOMEN"
 
 
 exit 0
