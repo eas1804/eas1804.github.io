43d8c9f69965aeaff3972387f4efa6b6a96c9057
Ievgen Slivak <eas1804@gmail.com>
2024-09-27 10:46:18 +0300

    new openVPN nft

--- Linux/OpenVPN/1-soft_net.sh
+++ Linux/OpenVPN/1-soft_net.sh
@@ -4,11 +4,23 @@ PORT=49859
 ETH=eth0
 
 echo "part 1. Install soft and config networt"
-apt update && apt install openvpn easy-rsa iptables curl iptables-persistent -y
+apt update && apt install openvpn easy-rsa curl  -y
 echo "net.ipv4.ip_forward=1" >>/etc/sysctl.conf && sysctl -p
-iptables -A INPUT -p udp -m udp --dport $PORT -j ACCEPT  -m comment --comment "For OpenVPN"
-iptables -t nat -A POSTROUTING -o $ETH -j MASQUERADE
-iptables -P FORWARD ACCEPT
-service netfilter-persistent save
-iptables -L INPUT -nv
-iptables -t nat -L POSTROUTING -nv
+
+
+
+/sbin/nft add table ip nat
+/sbin/nft add chain nat postrouting { type nat hook postrouting priority 0\; }
+/sbin/nft add rule ip nat postrouting oif $ETH masquerade 
+
+/sbin/nft add rule inet filter input udp dport $PORT counter accept
+/sbin/nft list ruleset
+
+exit 0
+#iptables -A INPUT -p udp -m udp --dport $PORT -j ACCEPT  -m comment --comment "For OpenVPN"
+#iptables -t nat -A POSTROUTING -o $ETH -j MASQUERADE
+#iptables -P FORWARD ACCEPT
+#apt install iptables-persistent iptables 
+#service netfilter-persistent save
+#iptables -L INPUT -nv
+#iptables -t nat -L POSTROUTING -nv
