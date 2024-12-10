63c9c27cd135f7d540a7a9c6370350968e5cdd0e
Ievgen Slivak <eas1804@gmail.com>
2024-12-10 15:47:39 +0200

    more

--- Linux/monitoring/Scrypts/refresh_home_mlp_pp_ua.sh
+++ Linux/monitoring/Scrypts/refresh_home_mlp_pp_ua.sh
@@ -3,8 +3,10 @@
 DOMEN=home.mlp.pp.ua
 
 RESULT=$(dig $DOMEN A +short)
-/usr/sbin/iptables -D INPUT -p tcp -s $RESULT -m tcp --dport 22 -j ACCEPT -m comment --comment "$DOMEN"  2>/dev/nul
-/usr/sbin/iptables -A INPUT -p tcp -s $RESULT -m tcp --dport 22 -j ACCEPT -m comment --comment "$DOMEN"
+
+nft -f /etc/nftables.conf
+nft add    rule inet filter input ip saddr $RESULT accept
+
 
 
 exit 0
--- Linux/monitoring/Scrypts/smartd_test.sh
+++ Linux/monitoring/Scrypts/smartd_test.sh
@@ -1,11 +1,10 @@
-  GNU nano 7.2                                                                       test.sh
 #!/bin/bash
 
 # telegramm env
 BOT_TOKEN="18045447450203:AAGEjU7HKEEGle1Zv2an5sVF49QxNxhw03w"
 CHAT_ID="33109821804"
 
-IP_global=$(curl 2ip.ru)
+IP_global=$(curl 2ip.com.ua)
 
 MESSAGE="Скрипт $echo $0
 Проблема с НОСИТЕЛЕМ данных
@@ -13,8 +12,8 @@ WAN ip  $IP_global
 "
 
 # Проверяем вывод команды journalctl -p 2 | grep smartd
-if journalctl -p 2 | grep -q smartd; then
-    echo "проблема"
+if journalctl -p 2  --since "2024-12-07 12:00:00"   | grep -q smartd; then
+    echo "проблема journalctl -p 2 | grep -q smartd  "
 curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&text=$MESSAGE" > /dev/null
 
 else
