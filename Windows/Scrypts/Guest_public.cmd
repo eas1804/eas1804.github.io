367e49cea9e444a2eb93f6353bbb77e0f3630e1c
Ievgen Slivak <eas1804@gmail.com>
2025-02-04 07:15:17 +0200

    index pbs

--- Windows/Scrypts/Guest_public.cmd
+++ Windows/Scrypts/Guest_public.cmd
@@ -1,3 +1,3 @@
+echo "зайти на сетевую шару где нет авторизаци"
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters" /v AllowInsecureGuestAuth /t REG_DWORD /d 1
-
 net stop LanmanWorkstation && net start LanmanWorkstation
